fa9930a14e7d177677b0f113bd3e0a8415eafd78
evan0Lee <stay136@gmail.com>
2024-08-02 14:02:28 +0900

    add clock

--- habbitApp/habbitApp/src/screens/MainScreen.tsx
+++ habbitApp/habbitApp/src/screens/MainScreen.tsx
@@ -1,5 +1,5 @@
 // src/screens/MainScreen.tsx
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RouteProp} from '@react-navigation/native';
@@ -15,9 +15,23 @@ type Props = {
 
 const MainScreen: React.FC<Props> = ({navigation, route}) => {
   const {username} = route.params;
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <View style={styles.container}>
+      <View style={styles.dateTimeContainer}>
+        <Text style={styles.dateTimeText}>
+          {currentDateTime.toLocaleDateString()}{' '}
+          {currentDateTime.toLocaleTimeString()}
+        </Text>
+      </View>
       <Text style={styles.title}>Welcome, {username}</Text>
       <Button title="Log out" onPress={() => navigation.navigate('Login')} />
     </View>
@@ -29,6 +43,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  dateTimeContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+  },
+  dateTimeText: {
+    fontSize: 16,
   },
   title: {
     fontSize: 24,
